c37e8d35cf5ff1bc0002ac0e81f2fb6775488e99
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2019-04-16 15:34:43 +0200

    Update imports JsonFormats.scala

--- app/model/JsonFormats.scala
+++ app/model/JsonFormats.scala
@@ -1,9 +1,7 @@
 package model
 
-import play.api.libs.json._
-import Consts._
 import model.levenshtein._
-import play.api.libs.functional.syntax._
+import play.api.libs.json._
 
 
 object JsonFormats {
